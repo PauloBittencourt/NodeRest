423841eb6255474020be2b68e7ffc7d317ae34f8
PauloBittencourt <pbittencourtjr@gmail.com>
2021-10-21 23:45:08 -0300

    Add token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,23 +1,55 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth')
 
 const User = require('../models/User')
 
 const router = express.Router()
 
+function generateToken(params = {}) {
+    return jwt.sign({ params }, authConfig.secret, {
+        expiresIn: 86400
+    })
+}
+
 router.post('/register', async (req, res) => {
     const { email } = req.body
 
     try {
-        if (await User.findOne({email}))
-        return res.status(400).send({ error: 'Usuário já existe !' })
+        if (await User.findOne({ email }))
+            return res.status(400).send({ error: 'Usuário já existe !' })
         const user = await User.create(req.body)
 
         user.password = undefined
 
-        return res.send({ user })
+        return res.send({
+            user,
+            token: generateToken({ id: user.id }),
+        })
     } catch (err) {
         return res.status(404).send({ error: ' O registro falhou !' })
     }
+})
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user)
+        return res.status(404).send({ error: 'Usuário não localizado.' })
+
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Senha Invalida' })
+
+    user.password = undefined
+
+    res.send({
+        user,
+        token: generateToken({ id: user.id }),
+    })
 })
 
 module.exports = app => app.use('/auth', router)
